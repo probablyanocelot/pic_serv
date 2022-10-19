df75ee10dd08bdbc240583d57633a8d86f485d23
ocelot <probablyanocelot@gmail.com>
2022-10-18 20:50:15 -0500

    picture model, stable

--- main.py
+++ main.py
@@ -16,45 +16,43 @@ db = SQLAlchemy(app)
 
 
 @dataclass
-class Product(db.Model):
+class Picture(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     title = db.Column(db.String(200))
     image = db.Column(db.String(200))
 
 
-@dataclass
-class ProductUser(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
-
-    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
-
-
-@ app.route('/api/products')
+@ app.route('/api/pictures')
 def index():
-    return jsonify(Product.query.all())
+    return jsonify(Picture.query.all())
+
+# @dataclass
+# class ProductUser(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer)
+#     product_id = db.Column(db.Integer)
+
+#     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 
-@app.route('/api/products/<int:id>/like')
-def like(id):
-    req = requests.get('http://docker.for.mac.localhost:8000/api/user')
-    json = req.json()
+# @app.route('/api/products/<int:id>/like')
+# def like(id):
+#     req = requests.get('http://docker.for.mac.localhost:8000/api/user')
+#     json = req.json()
 
-    try:
-        productUser = ProductUser(user_id=json['id'], product_id=id)
-        db.session.add(productUser)
-        db.session.commit()
+#     try:
+#         productUser = ProductUser(user_id=json['id'], product_id=id)
+#         db.session.add(productUser)
+#         db.session.commit()
 
-        publish('product_liked', id)
+#         publish('product_liked', id)
 
-    except:
-        abort(400, "You already liked this product")
+#     except:
+#         abort(400, "You already liked this product")
 
-    return jsonify({
-        'message': 'success',
-    })
-
+#     return jsonify({
+#         'message': 'success',
+#     })
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=7777)
